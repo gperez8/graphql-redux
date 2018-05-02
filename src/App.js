4ac376d7f8a82311c74c3fd1922036f3247f0863
Gregory <gregory.facyt@gmail.com>
2018-05-02 09:54:58 -0400

    show information and loading

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
 
-    console.log(this.props.data);
-
+    console.log('data', this.props.data);
+    const { rates } = this.props.data
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -18,6 +20,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        {this.props.data.loading &&
+            <div> 
+                <h1>LOADING ...</h1>
+            </div> 
+        }
+
+        {!this.props.data.loading && 
+            rates.map((rate, index) => <p key={index}> {rate.currency } </p>)
+        }
       </div>
     );
   }
